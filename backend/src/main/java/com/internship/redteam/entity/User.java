b7f8bced2e30c997fde228a627c6627049a75a4a
Santosh Chougule <chougulesantu01@gmail.com>
2026-04-25 14:16:09 +0530

    Day 2 — Entity, Repository, audit fields and DB config completed

---
+++ backend/src/main/java/com/internship/redteam/entity/User.java
@@ -0,0 +1,34 @@
+package com.internship.redteam.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    // Getters and Setters
+}
